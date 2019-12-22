4fb19339b090cf191ec9ee2b4eae4e99d6ea111d
github username <github email address>
2019-12-21 22:22:24 -0500

    adds initBindingsAndEventListeners

--- src/components/categorySelector.js
+++ src/components/categorySelector.js
@@ -1,3 +1,11 @@
 class CategorySelector {
-   
+   constructor() {
+      this.categories = []
+      this.initBindingsAndEventListeners()
+   }
+
+   initBindingsAndEventListeners() {
+      this.container = document.querySelector('#category-selector')
+      this.allCategoriesContainer = document.querySelector('#all-categories')
+   }
 }
