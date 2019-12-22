1896b1d6cff56b188e94d8ccda750bf17108417c
github username <github email address>
2019-12-21 22:03:10 -0500

    adds DOMContentLoaded

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// we want to set up a chain of events that will enable us to run this program
+
+document.addEventListener('DOMContentLoaded', () => {
+   const container = document.querySelector('#app-container')
+
+   // instantiate an app with that 
+   new ApplicationCache(container)
+})
