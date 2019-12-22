651e46480b08bf9d92259dc8ce592078bc615f28
github username <github email address>
2019-12-21 22:26:27 -0500

    adds fetch and load categories

--- src/components/categorySelector.js
+++ src/components/categorySelector.js
@@ -2,10 +2,18 @@ class CategorySelector {
    constructor() {
       this.categories = []
       this.initBindingsAndEventListeners()
+      this.fetchAndLoadCategories()
    }
 
    initBindingsAndEventListeners() {
       this.container = document.querySelector('#category-selector')
       this.allCategoriesContainer = document.querySelector('#all-categories')
    }
+
+   async fetchAndLoadCategories() {
+      this.categories = await Category.retrieveAll()
+      this.render()
+   }
+
+   
 }
